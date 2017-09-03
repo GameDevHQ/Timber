94877d28ae7ffd1b74c254bc0ed34f311c72287a
Valeryi Savich <relrin78@gmail.com>
2017-09-03 16:20:59 +0300

    Added rendering a background sprite

--- Timber/Timber/Timber.cpp
+++ Timber/Timber/Timber.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+
 #include "SFML/Graphics.hpp"
 
 using namespace sf;
@@ -9,6 +10,43 @@ int main()
 	// Create and open a SFML window
 	VideoMode vm(1920, 1080);
 	RenderWindow window(vm, "Timber", Style::Fullscreen);
+
+	// Get the background
+	Texture textureBackground;
+	textureBackground.loadFromFile("Resources/Graphics/background.png");
+
+	Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);
+	spriteBackground.setPosition(0, 0);
+
+	while(window.isOpen())
+	{
+		/*
+		**************************************************
+		Handle the players input
+		**************************************************
+		*/
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		/*
+		**************************************************
+		Update the scene
+		**************************************************
+		*/
+
+		/*
+		**************************************************
+		Draw the scene
+		**************************************************
+		*/
+		window.clear();
+
+		window.draw(spriteBackground);
+		window.display();
+	}
 
 	return 0;
 }
