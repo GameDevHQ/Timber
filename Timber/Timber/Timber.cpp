48a7d4170ae6b796908d1c5700987a1982d5d74c
Valeryi Savich <relrin78@gmail.com>
2017-09-04 12:19:11 +0300

    Added spawning tree branches

--- Timber/Timber/Timber.cpp
+++ Timber/Timber/Timber.cpp
@@ -7,6 +7,20 @@
 #include "HUD.h"
 
 
+void updateBranches(int seed);
+enum class side
+{
+    LEFT,
+    RIGHT,
+    NONE
+};
+
+const int NUM_BRANCHES = 6;
+Sprite branches[NUM_BRANCHES];
+side branchPositions[NUM_BRANCHES];
+
+
+
 int main()
 {
     // Create and open a SFML window
@@ -36,6 +50,20 @@ int main()
     float timeRemaining = 6.0f;     // Time remaining
 
     HUD hud(&isPaused, &score, &timeRemaining);
+
+    // Prepare branches
+    Texture textureBranch;
+    textureBranch.loadFromFile("Resources/Graphics/branch.png");
+
+    // Set the texture for each branch sprite
+    for (int i = 0; i < NUM_BRANCHES; i++)
+    {
+        branches[i].setTexture(textureBranch);
+        branches[i].setPosition(-2000, -2000);
+        // Set the sprite's origin to dead center
+        // We can then spin it round without changing its position
+        branches[i].setOrigin(220, 20);
+    }
 
     while(window.isOpen())
     {
@@ -77,6 +105,31 @@ int main()
             for (auto it = clouds.begin(); it != clouds.end(); it++)
                 it->update(window, timedelta);
 
+            // update the branch sprites
+            for (int i = 0; i < NUM_BRANCHES; i++)
+            {
+                float height = i * 150;
+                if (branchPositions[i] == side::LEFT)
+                {
+                    // Move the sprite to the left side
+                    branches[i].setPosition(610, height);
+                    // Flip the sprite round the other way
+                    branches[i].setRotation(180);
+                }
+                else if (branchPositions[i] == side::RIGHT)
+                {
+                    // Move the sprite to the right side
+                    branches[i].setPosition(1330, height);
+                    // Set the sprite rotation to normal
+                    branches[i].setRotation(0);
+                }
+                else
+                {
+                    // Hide the branch
+                    branches[i].setPosition(3000, height);
+                }
+            }
+
             hud.update(window, timedelta);
         }
 
@@ -91,6 +144,11 @@ int main()
             it->draw(window);
 
         tree.draw(window);
+        for (int i = 0; i < NUM_BRANCHES; i++)
+        {
+            window.draw(branches[i]);
+        }
+
         bee.draw(window);
         hud.draw(window);
 
@@ -98,4 +156,31 @@ int main()
     }
 
     return 0;
+}
+
+
+void updateBranches(int seed)
+{
+    // Move all the branches down one place
+    for (int j = NUM_BRANCHES - 1; j > 0; j--)
+    {
+        branchPositions[j] = branchPositions[j - 1];
+    }
+
+    // Spawn a new branch at position 0
+    // LEFT, RIGHT or NONE
+    srand(static_cast<unsigned int>(time(0) + seed));
+    int r = (rand() % 5);
+    switch (r)
+    {
+    case 0:
+        branchPositions[0] = side::LEFT;
+        break;
+    case 1:
+        branchPositions[0] = side::RIGHT;
+        break;
+    default:
+        branchPositions[0] = side::NONE;
+        break;
+    }
 }
